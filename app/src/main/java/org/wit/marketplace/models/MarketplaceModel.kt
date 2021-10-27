5f9d1f36cddb88ee3da57289dfacad2d5c155de7
Federica Fiorenza <20091413@mail.wit.ie>
2021-10-28 00:33:55 +0100

    Added Parcelize in MarketplaceModel

--- app/src/main/java/org/wit/marketplace/models/MarketplaceModel.kt
+++ app/src/main/java/org/wit/marketplace/models/MarketplaceModel.kt
@@ -1,6 +1,10 @@
 package org.wit.marketplace.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class MarketplaceModel(
     var title: String = "",
     var description: String = ""
-)
+): Parcelable
