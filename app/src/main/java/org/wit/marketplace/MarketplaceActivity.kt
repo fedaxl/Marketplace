0f9b5b6a025cbadc9c504cb6bce8d2425e5b0b1f
Federica Fiorenza <20091413@mail.wit.ie>
2021-10-27 22:25:57 +0100

    Layout - adding binding to add item button

--- app/src/main/java/org/wit/marketplace/MarketplaceActivity.kt
+++ app/src/main/java/org/wit/marketplace/MarketplaceActivity.kt
@@ -2,15 +2,34 @@ package org.wit.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import org.wit.marketplace.databinding.ActivityMarketplaceBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MarketplaceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMarketplaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_marketplace)
+
+        binding = ActivityMarketplaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
         i("Marketplace Activity started..")
+
+        binding.btnAdd.setOnClickListener() {
+            val itemTitle = binding.itemTitle.text.toString()
+            if (itemTitle.isNotEmpty()) {
+                i("add Button Pressed: $itemTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
